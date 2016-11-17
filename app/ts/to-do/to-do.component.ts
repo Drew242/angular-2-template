2f039bc64bd31e71a3739a7414c1c35c6e59cb23
Drew Conly <drew.conly@cherwell.com>
2016-11-16 17:19:40 -0700

    angular 2 with bootstrap to-do crud

--- app/ts/to-do/to-do.component.ts
+++ app/ts/to-do/to-do.component.ts
@@ -1,9 +1,37 @@
 import { Component } from "angular2/core";
+import { Todo } from "./to-do";
+import { TodoService } from "./to-do.service";
 
 @Component({
   selector: 'to-do',
   templateUrl: 'app/ts/to-do/to-do.component.html',
-  styleUrls: [ 'app/ts/to-do/to-do.component.scss' ]
+  styleUrls: [ 'app/ts/to-do/to-do.component.scss' ],
+  providers: [ TodoService ]
 })
 
-export class ToDoComponent {}
+export class ToDoComponent {
+
+  newTodo: Todo = new Todo();
+
+  constructor(private todoService: TodoService) {
+  }
+
+  toggleTodoComplete(todo) {
+    this.todoService.toggleTodoComplete(todo);
+  }
+
+  addTodo() {
+    this.todoService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  removeTodo(todo) {
+    this.todoService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoService.getAllTodos();
+  }
+
+
+}
